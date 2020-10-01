86944717c0fb7f7a640e66e1f6370f9c63b40e5e
raudelf <rawdell@outlook.com>
2020-09-30 21:32:07 -0500

    :white_check_mark: searching.py
    Completed the linear_search and binary_search functions

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,8 @@
 def linear_search(arr, target):
     # Your code here
-
+    for i in range(0, len(arr)):
+        if arr[i] == target:
+            return i
 
     return -1   # not found
 
@@ -8,7 +10,19 @@ def linear_search(arr, target):
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
 
-    # Your code here
+    min = 0
+    max = len(arr) - 1
 
+    while min <= max:
+        middle = (min+max)//2
+        
+        if target < arr[middle]:
+            max = middle - 1
+
+        elif target > arr[middle]:
+            min = middle + 1
+
+        else:
+            return middle 
 
     return -1  # not found
